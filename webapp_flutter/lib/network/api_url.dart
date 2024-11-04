de365914c1e37796a57e4aa43e4ed93574a39c8a
otaviomart1ns <taviomartins01@gmail.com>
2024-11-04 07:28:02 -0300

    hotfix: Update API URL to use service name 'api' instead of localhost

--- webapp_flutter/lib/network/api_url.dart
+++ webapp_flutter/lib/network/api_url.dart
@@ -1,5 +1,5 @@
 abstract class ApiUrl {
-  static const String baseUrl = 'http://localhost:5000';
+  static const String baseUrl = 'http://api:5000';
   static const String productsUrl = '$baseUrl/produtos';
   static const String lojasUrl = '$baseUrl/lojas';
 }
